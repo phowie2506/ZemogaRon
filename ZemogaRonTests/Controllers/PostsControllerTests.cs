b422b9ea649bcb454fe4bc777e5b662aadb74e5c
Ronnie <riverav.ronnie@gmail.com>
2021-09-04 12:48:24 -0500

    unit test

--- ZemogaRonTests/Controllers/PostsControllerTests.cs
+++ ZemogaRonTests/Controllers/PostsControllerTests.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using ZemogaRon.Models;
 
 namespace ZemogaRon.Controllers.Tests
 {
@@ -12,9 +14,18 @@ namespace ZemogaRon.Controllers.Tests
     public class PostsControllerTests
     {
         [TestMethod()]
-        public void GetByUserIdTest()
+        public void ValidateModel()
         {
-            Assert.Fail();
+            //validate if the model is not valid , is required UserId
+            Post post = new Post()
+            {
+                Id = 1,
+                Title = "title of the post"
+            };
+            var validationResults = new List<ValidationResult>();
+            var context = new ValidationContext(post, serviceProvider: null, items: null);
+            bool isValid = Validator.TryValidateObject(post,context, validationResults, true);
+            Assert.IsFalse(isValid);
         }
     }
 }
